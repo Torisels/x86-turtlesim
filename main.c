8cdc9c9fba82732b9791b61a0cdc106ab7f6bd40
Gustaw Daczkowski <01149695@pw.edu.pl>
2020-05-10 16:28:59 +0200

    Change config.

--- main.c
+++ main.c
@@ -9,8 +9,8 @@
 /*
  * Important constants
  */
-#define INPUT_FILE_NAME "input.bin"
-#define OUTPUT_FILE_NAME "output.bmp"
+#define INPUT_FILE_NAME "input_1.bin"
+#define OUTPUT_FILE_NAME "output_1.bmp"
 #define CONFIG_FILE_NAME "config.txt"
 #define CONFIG_BUFFER_LEN 255 //config.txt buffer len (not important)
 
